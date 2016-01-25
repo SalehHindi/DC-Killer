12f53e806d5b8256235b870ff1a86cc1b0628b28
shindi <shindi@haverford.edu>
2016-01-25 00:45:35 -0500

    Added items to food nutrition database.

--- database/data.py
+++ database/data.py
@@ -4,8 +4,10 @@ This is the part of the program responsible for all database related operations 
 # TODO: 3 Write script that converts nutirition info as in foodbase.txt to a easily manipulatable database format
 # TODO: 3 Generate a food_list document for the DC Collection
 # TODO: 2 Document the design of the database
-import pymongo as mongo
+#import pymongo as mongo
 import datetime
+import json
+
 
 class Data(object):
     '''
@@ -21,21 +23,19 @@ class Data(object):
         if d: foo
         else: return 'error!'
         '''
-        if 1:
-            newConnect = mongo.MongoClient()              #A connection
-            DCKiller = newConnect['DCKillerdb']       #A Database
 
-            DCMeals = DCKiller['DCMeals']             #A Collection
-            HaffnerMeals = DCKiller['HaffnerMeals']   #A Collection
-            ErdmanMeals = DCKiller['ErdmanMeals']     #A Collection
-            SpecialMeals = DCKiller['Special Meals']  #A Collection
+        '''
+        newConnect = mongo.MongoClient()          #A connection
+        DCKiller = newConnect['DCKillerdb']       #A Database
 
-            self.Collections={'DC':DCMeals, 'Haffner':HaffnerMeals, 'Erdman':ErdmanMeals, 'Special': SpecialMeals}
-         
-           
-       # except:
-       #     print 'Database connection error!'
-           
+        DCMeals = DCKiller['DCMeals']             #A Collection
+        HaffnerMeals = DCKiller['HaffnerMeals']   #A Collection
+        ErdmanMeals = DCKiller['ErdmanMeals']     #A Collection
+        SpecialMeals = DCKiller['SpecialMeals']  #A Collection
+        NutritionCollection = DCKiller['NutritionCollection']
+
+        self.Collections={'DC':DCMeals, 'Haffner':HaffnerMeals, 'Erdman':ErdmanMeals, 'Special': SpecialMeals, 'NutritionCollection':NutritionCollection}
+        '''
 
     def today_date(self):
         '''
@@ -82,18 +82,55 @@ class Data(object):
         entry = self.Coollections.get_one({'_id':today})
         return entry
 
+    def add_food_JSON_data_to_database(self, number_of_foods):
+        '''
+
+        takes number_of_foods which is the number of *.txt files in /json_files
+        :return: nothing. Adds all the foods in /json_files to the database
+        '''
+        self.change_paren(number_of_foods)
+        for fileindex in range(number_of_foods):
+            pass
+            food = []
+            afile = open('json_files/JSON_' +str(fileindex + 1) + '.txt')
+            food = afile.readline()
+            food = json.loads(food)
+            print food
+
+            #for each JSON element, create a list of all the information for each food
+            #add that element to the list
+            #make id==name
+            #add the temporary list into the database
+
+    def change_paren(self, number):
+        '''
+        Goes thru /json_files and converts all single parentheses to double parentheses for the JSON library
+        :return:
+        '''
+        pass
+        for index in range(number):
+            afile = open('json_files/JSON_' + str(index + 1) + '.txt', "r")
+            filecontents = afile.read()
+            afile.close()
+
+            afile = open('json_files/JSON_' + str(index + 1) + '.txt', "w")
+            filecontents = filecontents.replace('\'', '"')
+            afile.write(filecontents)
+        #Load the contents of the file into a string
+        #Replace ' with "
+        #Write file
+
 test=Data()
-a={'breakfast':[['7am-9am'], ['pancakes']*5, ['600']*5], 
-'lunch': [['1pm-2pm'], ['bagels']*5, ['500']*5]}
-b={'breakfast':[['7am-9am'], ['pes']*5, ['700']*5], 
-'lunch': [['1pm-2pm'], ['bls']*5, ['800']*5]}
-c={'breakfast':[['7am-9am'], ['paijijoncakes']*5, ['900']*5], 
-'lunch': [['1pm-2pm'], ['bojpokpoagels']*5, ['1000']*5]}
+test.add_food_JSON_data_to_database(100)
+
 #test.new_entry('DC', a)
 #test.new_entry('DC', b, date=20160103)
 #test.new_entry('DC', c, date=20160102)
 
+
+
 x= test.Collections['DC'].find()
 for i in x:
     if 'lunch' in i:
-        print i['lunch']
+        #print i['lunch']
+        pass
